ba1fb78eebb079c2b407ce3d96be16fb03a070f2
Rasmus Buchmann <rasmus.buchmann@googlemail.com>
2022-09-28 23:59:19 +0200

    Version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rbuchmann/trofit "1.0.0"
+(defproject org.clojars.rbuchmann/trofit "1.0.1-SNAPSHOT"
   :description "A simple macro to make defining the re-frame boilerplate more convenient"
   :url "https://github.com/rbuchmann/trofit"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
